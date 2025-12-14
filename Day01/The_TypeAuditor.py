6b2afb3bb57ec41146dfad62c1ae5d6be0c63051
RiyaRoy06 <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-14 22:55:12 +0600

    Update The_TypeAuditor.py

--- Day01/The_TypeAuditor.py
+++ Day01/The_TypeAuditor.py
@@ -1,1 +1,12 @@
+# Goal: Ask the user to type a number.
+#      1. Print the type() of the raw input.
+#      2. Cast it to a float.
+#      3. Print the type() again. 
 
+num = input("Enter a number : ")
+print(type(num))
+try:
+   new_num=float(num)
+   print(type(new_num))
+except Error:
+  print("Invalid number.")
