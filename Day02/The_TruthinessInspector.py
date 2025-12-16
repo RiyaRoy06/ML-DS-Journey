67633a866b15b8a376ac00fb006679f09836ab41
Suyeata Roy Riya <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-16 21:08:35 +0600

    Create The_TruthinessInspector.py

---
+++ Day02/The_TruthinessInspector.py
@@ -0,0 +1,7 @@
+# Goal: Create an variable data=[] (Empty list). Write an if statement to print "Data Found" or "No Data" without compare it to anything(e.g., do not use len(data)>0).
+
+data=[]
+if bool(data)==False:
+  print("No Data")
+else :
+  print("Data Found")
