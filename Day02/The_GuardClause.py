d15658fd6c2778c15e98b682a64c06971b63acd9
Suyeata Roy Riya <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-16 19:54:52 +0600

    Update The_GuardClause.py

--- Day02/The_GuardClause.py
+++ Day02/The_GuardClause.py
@@ -1,1 +1,10 @@
+# Goal: Create a variable user=None. Write an if statement that checks if user is None And if user has an "admin" access.
 
+user=None
+try:
+  if user is not None and user=="admin":
+    print("Access.")
+  else :
+    print("Does not access")
+except Error:
+  print("Error")
