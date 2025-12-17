b5f096f7f1dac8b22b04ff9ec6abbaf23d3e5508
Suyeata Roy Riya <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-17 11:21:50 +0600

    Update The_InfiniteGuardian.py

--- Day03/The_InfiniteGuardian.py
+++ Day03/The_InfiniteGuardian.py
@@ -1,1 +1,6 @@
+# Goal: Write a script that askas the user for password repeatedly. It must run forever until the user types "stop".
 
+while True:
+  pwd=input("Set Password: ")
+  if pwd=="stop":
+    break
