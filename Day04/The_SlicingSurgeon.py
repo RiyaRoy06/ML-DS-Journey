df982cb72baeef8ba646a4470d03cb34d54deedb
Suyeata Roy Riya <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-18 19:40:30 +0600

    Create The_SlicingSurgeon.py

---
+++ Day04/The_SlicingSurgeon.py
@@ -0,0 +1,5 @@
+# Goal: Create a list data=[10,20,30,40,50]. Create a new list containing the last three items in reverse order.
+
+data=[10,20,30,40,50]
+list=data[-1:-4:-1]
+print("new list: ",list)
