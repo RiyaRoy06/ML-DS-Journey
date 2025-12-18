049c9525c6350aefdf976c0c92dcbde18a91a769
Suyeata Roy Riya <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-18 19:51:06 +0600

    Create The_StackEmulator.py

---
+++ Day04/The_StackEmulator.py
@@ -0,0 +1,8 @@
+# Goal: Create an empty list. Add numbers 1,2,3. Then remove the last number (3) and print it.
+
+list=[]
+list.append(1)
+list.append(2)
+list.append(3)
+list.pop()
+print("final_list: ",list)
