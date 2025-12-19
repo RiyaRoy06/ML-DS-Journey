794c7faabe6c0bd8d8d4130bb33f334a6a856eec
Suyeata Roy Riya <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-19 22:45:13 +0600

    Create The_SpeedTrap.py

---
+++ Day05/The_SpeedTrap.py
@@ -0,0 +1,6 @@
+# Goal: Create a list of 1 million numbers. Create a set(hash table) of the same numbers. Check if the number -1 exists in both.
+
+List=list(range(1000001))
+Set=set(List)
+print(-1 in List)
+print(-1 in Set)
