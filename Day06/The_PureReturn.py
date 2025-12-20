aa817cd2a2d135661ee1eec812a541a5fc276b2e
Suyeata Roy Riya <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-20 21:59:55 +0600

    Create The_PureReturn.py

---
+++ Day06/The_PureReturn.py
@@ -0,0 +1,7 @@
+# Goal: Write a function add(a,b) that prints the sum but returns nothing. Assign the result to a variable res=add(5,5). Print res.
+
+def add(a,b):
+    (a+b)
+    return None
+res=add(5,5)
+print(res)
