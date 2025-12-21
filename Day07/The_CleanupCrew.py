8276c9e5172d507a4c614aa126e4ed330a8dd1cf
Suyeata Roy Riya <163673366+RiyaRoy06@users.noreply.github.com>
2025-12-21 22:05:18 +0600

    Update The_CleanupCrew.py

--- Day07/The_CleanupCrew.py
+++ Day07/The_CleanupCrew.py
@@ -1,1 +1,10 @@
+# Goal: Write a try/except block that divides two numbers. Add a finally block that print "Execusion Complete" regardless of whether the division succeeded or failed.
 
+x=int(input("Enter first number: "))
+y=int(input("Enter second number: "))
+try:
+  print(x/y)
+except ZeroDivisionError:
+  print("Cannot divide by zero.")
+finally:
+  print("Execution Complete")
